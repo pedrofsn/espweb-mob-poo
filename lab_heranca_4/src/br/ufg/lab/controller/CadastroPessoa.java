1cbaeb69f8f5b3428d4c096eb0a057045659269a
Pedrofsn <pedrokra@gmail.com>
2017-05-13 15:30:40 -0300

    Incrementando o cadastro de pessoas

--- lab_heranca_4/src/br/ufg/lab/controller/CadastroPessoa.java
+++ lab_heranca_4/src/br/ufg/lab/controller/CadastroPessoa.java
@@ -1,8 +1,12 @@
 package br.ufg.lab.controller;
 
+import br.ufg.lab.model.Cliente;
+import br.ufg.lab.model.Funcionario;
+import br.ufg.lab.model.Gerente;
 import br.ufg.lab.model.Pessoa;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -24,10 +28,59 @@ public class CadastroPessoa {
     }
 
     public void imprimirRelatorio() {
-        System.out.println("\nO total de pessoas cadastradas no sistema é de " + getQtdAtual());
+        printarQuantidades();
+        printarMaiorSalario();
     }
 
-    public int getQtdAtual() {
-        return qtdAtual;
+    private void printarMaiorSalario() {
+        float maiorSalario = 0;
+        Funcionario funcionarComMaiorSalario = null;
+
+        for (Pessoa pessoa : pessoasCadastradas) {
+            if (pessoa instanceof Funcionario) {
+
+                Funcionario funcionario = (Funcionario) pessoa;
+
+                if (maiorSalario == 0 || funcionario.getSalario() > maiorSalario) {
+                    maiorSalario = funcionario.getSalario();
+                    funcionarComMaiorSalario = funcionario;
+                }
+
+            }
+        }
+
+        if (funcionarComMaiorSalario != null) {
+            System.out.println(funcionarComMaiorSalario.getNome() + " tem o maior salário (" + maiorSalario + ")");
+        } else {
+            System.out.println("Você não possui funcionários cadastrados");
+        }
+
+    }
+
+    private void printarQuantidades() {
+        Iterator iterator = pessoasCadastradas.iterator();
+        int contadorCliente = 0;
+        int contadorGerente = 0;
+        int contadorFuncionario = 0;
+
+        while (iterator.hasNext()) {
+            Pessoa pessoa = (Pessoa) iterator.next();
+
+            if (pessoa instanceof Cliente) {
+                ++contadorCliente;
+
+            } else if (pessoa instanceof Gerente) {
+                ++contadorGerente;
+
+            } else if (pessoa instanceof Funcionario) {
+                ++contadorFuncionario;
+
+            }
+        }
+
+        System.out.println("\nQuantidade de cliente(s): " + contadorCliente);
+        System.out.println("\nQuantidade de gerente(s): " + contadorGerente);
+        System.out.println("\nQuantidade de funcionário(s): " + contadorFuncionario);
+        System.out.println("\nQuantidade total: " + qtdAtual);
     }
 }
